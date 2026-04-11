6a500668c452142cfa7b6242f359f90bdd6753a0
Rafael Belentani <rafaelluiz_belentani@hotmail.com>
2026-04-11 20:16:30 -0300

    corrigindo requirements

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import json
 import pandas as pd
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -13,7 +14,7 @@ st_autorefresh(interval=10000, key="refresh")
 # 🔐 FIREBASE (corrigido)
 # ==============================
 if not firebase_admin._apps:
-    cred = credentials.Certificate("PI5.json")
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
